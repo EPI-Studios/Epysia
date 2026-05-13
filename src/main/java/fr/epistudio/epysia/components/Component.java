53f908989d5afc79f66722fe41b6883a09b74328
mrqsdf <maqw@free.fr>
2026-05-13 13:08:39 +0200

    Component class

---
+++ src/main/java/fr/epistudio/epysia/components/Component.java
@@ -0,0 +1,4 @@
+package fr.epistudio.epysia.components;
+
+public abstract class Component implements IComponent {
+}
---
+++ src/main/java/fr/epistudio/epysia/components/IComponent.java
@@ -0,0 +1,4 @@
+package fr.epistudio.epysia.components;
+
+public interface IComponent {
+}
