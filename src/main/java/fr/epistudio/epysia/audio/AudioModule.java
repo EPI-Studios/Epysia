2d9ef9101e883508957590f33487a789e5f47274
Meekiavelique <meekiavelique@gmail.com>
2026-05-24 15:35:42 +0200

    feat(audio): expose audio as an engine module that opens openal and registers the system

---
+++ src/main/java/fr/epistudio/epysia/audio/AudioModule.java
@@ -0,0 +1,18 @@
+package fr.epistudio.epysia.audio;
+
+import fr.epistudio.epysia.EngineModule;
+import fr.epistudio.epysia.SystemRegistry;
+import fr.epistudio.epysia.logging.ConsoleLogger;
+
+public final class AudioModule implements EngineModule {
+
+    @Override
+    public int order() {
+        return 200;
+    }
+
+    @Override
+    public void registerSystems(SystemRegistry registry) {
+        registry.add(new AudioSystem(new ConsoleLogger()));
+    }
+}
