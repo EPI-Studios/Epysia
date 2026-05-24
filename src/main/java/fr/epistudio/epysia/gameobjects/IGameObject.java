7843ea47e6a6118f6f5977b59698013877b8d525
Meekiavelique <meekiavelique@gmail.com>
2026-05-24 15:35:39 +0200

    refactor(gameobjects): align the game object interface with the new api

--- src/main/java/fr/epistudio/epysia/gameobjects/IGameObject.java
+++ src/main/java/fr/epistudio/epysia/gameobjects/IGameObject.java
@@ -2,50 +2,15 @@ package fr.epistudio.epysia.gameobjects;
 
 import fr.epistudio.epysia.components.IComponent;
 
-/**
- * GameObject Interface
- */
+import java.util.Optional;
+
 public interface IGameObject {
 
-    /**
-     * get Component
-     * @param componentClass components class to get
-     * @return Component or null if not found
-     * @param <T> Component type
-     */
-    <T extends IComponent> T getComponent(Class<T> componentClass);
+    String name();
 
-    /**
-     * add Component
-     * @param component component to add
-     * @return added component
-     * @param <T> Component type
-     */
+    <T extends IComponent> Optional<T> getComponent(Class<T> componentClass);
+
     <T extends IComponent> T addComponent(T component);
 
-    /**
-     * remove Component
-     * @param componentClass components class to remove
-     * @return removed component or null if not found
-     * @param <T> Component type
-     */
-    <T extends IComponent> T removeComponent(Class<T> componentClass);
-
-    /**
-     * onInit
-     */
-    void onInit();
-
-    /**
-     * onUpdate
-     * @param dt delta time since last update
-     */
-    void onUpdate(float dt);
-
-    /**
-     * onDestroy
-     */
-    void onDestroy();
-
-    String getName();
+    <T extends IComponent> Optional<T> removeComponent(Class<T> componentClass);
 }
