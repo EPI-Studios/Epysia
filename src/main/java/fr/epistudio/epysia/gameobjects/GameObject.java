c8d7b185c48ffdc3ab44a73970566173590d78a8
Meekiavelique <meekiavelique@gmail.com>
2026-05-24 15:35:39 +0200

    refactor(gameobjects): rewire game objects against the new module and system lifecycle

--- src/main/java/fr/epistudio/epysia/gameobjects/GameObject.java
+++ src/main/java/fr/epistudio/epysia/gameobjects/GameObject.java
@@ -1,92 +1,68 @@
 package fr.epistudio.epysia.gameobjects;
 
-import fr.epistudio.epysia.components.Component;
 import fr.epistudio.epysia.components.IComponent;
-import fr.epistudio.epysia.components.transforms.Transform;
 import fr.epistudio.epysia.exceptions.ComponentPresentException;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
-public class GameObject implements IGameObject {
+public final class GameObject implements IGameObject {
 
-    private Set<IComponent> components;
-    private String name;
-    private transient Transform transform;
+    private final String name;
+    private final Map<Class<?>, IComponent> componentsByType = new HashMap<>();
 
-    public GameObject(String name, Transform transform){
-        this.components = new HashSet<>();
+    public GameObject(String name) {
         this.name = name;
-        this.transform = transform;
-        components.add(transform);
-    }
-
-    public final void Init() {
-
-        onInit();
     }
 
     @Override
-    public void onInit() {
-
-    }
-
-    public final void update(float dt) {
-        for (IComponent component : components) {
-            ((Component) component).update(dt);
-        }
-        onUpdate(dt);
+    public String name() {
+        return name;
     }
 
     @Override
-    public void onUpdate(float dt) {
-
-    }
-
-    public final void destroy() {
-        for (IComponent component : components){
-            ((Component) component).destroy();
-        }
-        onDestroy();
-    }
-
-    @Override
-    public void onDestroy() {
-
-    }
-
-
-    @Override
-    public <T extends IComponent> T getComponent(Class<T> componentClass) {
-        for (IComponent component : components) {
-            if (componentClass.isInstance(component)) {
-                return componentClass.cast(component);
-            }
-        }
-        return null;
+    @SuppressWarnings("unchecked")
+    public <T extends IComponent> Optional<T> getComponent(Class<T> componentClass) {
+        return Optional.ofNullable((T) componentsByType.get(componentClass));
     }
 
     @Override
     public <T extends IComponent> T addComponent(T component) {
-        if (getComponent(component.getClass()) != null) {
+        if (componentsByType.containsKey(component.getClass())) {
             throw new ComponentPresentException(component, this);
         }
-        component.setGameObject(this);
-        components.add(component);
+        component.attachTo(this);
+        registerUnderHierarchy(component);
         return component;
     }
 
-    @Override
-    public <T extends IComponent> T removeComponent(Class<T> componentClass) {
-        T component = getComponent(componentClass);
-        if (component != null) {
-            components.remove(component);
+    private void registerUnderHierarchy(IComponent component) {
+        Class<?> currentClass = component.getClass();
+        while (currentClass != null && IComponent.class.isAssignableFrom(currentClass)) {
+            componentsByType.putIfAbsent(currentClass, component);
+            currentClass = currentClass.getSuperclass();
         }
-        return component;
     }
 
     @Override
-    public String getName() {
-        return name;
+    @SuppressWarnings("unchecked")
+    public <T extends IComponent> Optional<T> removeComponent(Class<T> componentClass) {
+        IComponent component = componentsByType.get(componentClass);
+        if (component == null) {
+            return Optional.empty();
+        }
+        unregisterUnderHierarchy(component);
+        return Optional.of((T) component);
+    }
+
+    private void unregisterUnderHierarchy(IComponent component) {
+        Class<?> currentClass = component.getClass();
+        while (currentClass != null && IComponent.class.isAssignableFrom(currentClass)) {
+            if (componentsByType.get(currentClass) == component) {
+                componentsByType.remove(currentClass);
+            }
+            currentClass = currentClass.getSuperclass();
+        }
     }
 }
