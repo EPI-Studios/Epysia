553fcabf473bed43dfd1178a2092e9a92eb12ea0
Meekiavelique <meekiavelique@gmail.com>
2026-05-24 15:35:39 +0200

    refactor(components): turn the base component into a shared marker type

--- src/main/java/fr/epistudio/epysia/components/Component.java
+++ src/main/java/fr/epistudio/epysia/components/Component.java
@@ -3,56 +3,22 @@ package fr.epistudio.epysia.components;
 import fr.epistudio.epysia.exceptions.EpysiaException;
 import fr.epistudio.epysia.gameobjects.GameObject;
 
+import java.util.Optional;
+
 public abstract class Component implements IComponent {
 
-    protected transient GameObject gameObject;
-
-    public final void Init() {
-
-        onInit();
-    }
+    private GameObject gameObject;
 
     @Override
-    public void onInit() {
-
-    }
-
-    public final void update(float dt) {
-
-        onUpdate(dt);
-    }
-
-    @Override
-    public void onUpdate(float dt) {
-
-    }
-
-    public final void destroy() {
-
-        onDestroy();
-    }
-
-    @Override
-    public void onDestroy() {
-
-    }
-
-    @Override
-    public void setGameObject(GameObject gameObject) {
+    public final void attachTo(GameObject gameObject) {
         if (this.gameObject != null) {
-            throw new EpysiaException("GameObject is already set for this component.");
+            throw new EpysiaException("Component is already attached to a GameObject.");
         }
         this.gameObject = gameObject;
     }
 
     @Override
-    public GameObject getGameObject() {
-        return gameObject;
+    public final Optional<GameObject> owner() {
+        return Optional.ofNullable(gameObject);
     }
-
-    @Override
-    public boolean hasGameObject() {
-        return gameObject != null;
-    }
-
 }
