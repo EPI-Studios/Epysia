8aad1ce14eafc873f6f91c265da755f8070f96f5
Meekiavelique <meekiavelique@gmail.com>
2026-07-19 15:41:59 +0200

    inspector panel

---
+++ epysia-editor/src/main/java/fr/epistudio/epysia/editor/inspector/AssetMimeTypes.java
@@ -0,0 +1,27 @@
+package fr.epistudio.epysia.editor.inspector;
+
+import fr.epistudio.epysia.render.backend.TextureHandle;
+import fr.epistudio.epysia.render.mesh.UploadedMesh;
+
+public final class AssetMimeTypes {
+
+    public static final String MESH = "asset/mesh";
+    public static final String TEXTURE = "asset/texture";
+    public static final String AUDIO = "asset/audio";
+    public static final String PREFAB = "asset/prefab";
+    public static final String SCENE = "asset/scene";
+    public static final String NONE = "";
+
+    private AssetMimeTypes() {
+    }
+
+    public static String forAssetType(Class<?> type) {
+        if (type == UploadedMesh.class) {
+            return MESH;
+        }
+        if (type == TextureHandle.class) {
+            return TEXTURE;
+        }
+        return NONE;
+    }
+}
