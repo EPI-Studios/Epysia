21f373b6b98353d92e0908aafdcf5aaf81da233b
Meekiavelique <meekiavelique@gmail.com>
2026-07-21 17:00:11 +0200

    gltf importer writes epymaterial files and extracts textures

--- epysia-editor/src/main/java/fr/epistudio/epysia/editor/importer/GltfImportResult.java
+++ epysia-editor/src/main/java/fr/epistudio/epysia/editor/importer/GltfImportResult.java
@@ -3,5 +3,5 @@ package fr.epistudio.epysia.editor.importer;
 import java.nio.file.Path;
 import java.util.List;
 
-public record GltfImportResult(List<Path> meshFiles, List<Path> clipFiles, List<String> warnings) {
+public record GltfImportResult(List<Path> meshFiles, List<Path> clipFiles, List<Path> materialFiles, List<String> warnings) {
 }
